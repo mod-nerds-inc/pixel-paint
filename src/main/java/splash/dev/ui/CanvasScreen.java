7276989dd72aa21e4e9bd6ac7b4483f633d1c549
SplashAni <106507600+SplashAni@users.noreply.github.com>
2025-06-26 22:44:12 +0200

    ok gang this is good enough gn

--- src/main/java/splash/dev/ui/CanvasScreen.java
+++ src/main/java/splash/dev/ui/CanvasScreen.java
@@ -19,7 +19,7 @@ public class CanvasScreen extends Screen {
         int centerY = (this.height / 2) - (panelHeight / 2);
 
         int bg = new Color(35, 35, 35, 255).getRGB();
-        // coding from a ms paint pic is god tier
+
         context.fill(
                 centerX,
                 centerY,
@@ -37,35 +37,57 @@ public class CanvasScreen extends Screen {
 
         int squareX = 0;
 
-//        int white = Color.WHITE.getRGB();
-        int red = Color.WHITE.getRGB();
-        int black =Color.WHITE.getRGB();
+        int c = new Color(255, 255, 255, 134).getRGB();
+
+        int hoveredSquare = -1;
+        if (mouseX >= squareX && mouseX <= squareX + panelWidth &&
+                mouseY >= startY && mouseY <= startY + squaresCount * squareHeight) {
+            hoveredSquare = (mouseY - startY) / squareHeight;
+        }
 
         for (int i = 0; i < squaresCount; i++) {
             int squareY = startY + squareHeight * i;
+            int halfWidth = panelWidth / 2;
+
+            int leftFillColor = bg;
+            int rightFillColor = bg;
+
+            if (i == hoveredSquare) {
+                if (mouseX < squareX + halfWidth) {
+                    leftFillColor = new Color(255, 0, 0, 134).getRGB();
+                } else {
+                    rightFillColor = new Color(255, 0, 0, 134).getRGB();
+                }
+            }
 
             context.fill(
                     squareX,
                     squareY,
-                    squareX + panelWidth,
+                    squareX + halfWidth,
                     squareY + squareHeight,
-                    bg
+                    leftFillColor
             );
 
-            context.fill(squareX, squareY, squareX + panelWidth, squareY + 1, red);
-//            context.fill(squareX, squareY + squareHeight - 1, squareX + squareWidth, squareY + squareHeight, red);
-           /* context.fill(squareX, squareY, squareX + 1, squareY + squareHeight, red);
-            context.fill(squareX + squareWidth - 1, squareY, squareX + squareWidth, squareY + squareHeight, red);
-*/
-            int centerLineX = (squareX + panelWidth / 2)-1;
+            context.fill(
+                    squareX + halfWidth,
+                    squareY,
+                    squareX + panelWidth,
+                    squareY + squareHeight,
+                    rightFillColor
+            );
+
+            context.fill(squareX, squareY, squareX + panelWidth, squareY + 1, c);
+
+            int centerLineX = squareX + halfWidth - 1;
             context.fill(
                     centerLineX,
                     squareY-15,
                     centerLineX + 1,
-                    squareY + squareHeight+5,
-                    black
+                    squareY + squareHeight+15,
+                    c
             );
         }
+
 
         super.render(context, mouseX, mouseY, delta);
     }
